37fbedceadb3d8dd1da79421d7eff5d30a2450c0
Ctrix <csk77@pm.me>
2022-03-12 19:43:24 +0530

    another build error

--- src/components/UI/SideBarHeader.js
+++ src/components/UI/SideBarHeader.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { signOut, getAuth } from "firebase/auth";
 import AppContext from "../GlobalStore/Context";
 import { useNavigate } from "react-router-dom";
